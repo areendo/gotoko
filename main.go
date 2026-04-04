94880e2ef721abadd1184c4febf637a4f29b52b7
areendo <33369686+areendo@users.noreply.github.com>
2026-04-04 09:52:30 +0000

    reset simple version

--- main.go
+++ main.go
@@ -1,27 +1,21 @@
+
 package main
 
 import (
-	"database/sql"
-	"fmt"
+	"html/template"
 	"net/http"
-	"os"
-
-	"gotoko/handlers"
-
-	_ "modernc.org/sqlite"
 )
 
 func main() {
-	db, _ := sql.Open("sqlite", "./toko.db")
-	os.Mkdir("uploads", os.ModePerm)
+	// serve static
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 
-	handlers.SetDB(db)
+	// route
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("templates/index.html"))
+		tmpl.Execute(w, nil)
+	})
 
-	http.Handle("/", http.FileServer(http.Dir("./templates")))
-	http.Handle("/uploads/", http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads"))))
-
-	http.HandleFunc("/api/products", handlers.GetProducts)
-
-	fmt.Println("jalan di http://localhost:8080")
+	println("Server running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
